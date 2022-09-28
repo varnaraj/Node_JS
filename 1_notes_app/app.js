a24599fdd611b7f418b8469fea1ad629ee981fb8
varnaraj <varnaraj28@gmail.com>
2022-09-28 14:48:39 +0530

    update

--- 1_notes_app/app.js
+++ 1_notes_app/app.js
@@ -21,11 +21,19 @@
 
 console.log(process.argv[3]);
 import chalk from "chalk";
+import { argv } from "process";
+import yargs from "yargs";
+
+//const yargs=require ("yargs");
+
+
 //const getNotes=require("./notes.js");
-const command=process.argv[2]
-if(command==='add'){
-    console.log("adding note");
-}else if(command==='remove'){
-    console.log("removing note");
-}
+
+// if(command==='add'){
+//     console.log("adding note");
+// }else if(command==='remove'){
+//     console.log("removing note");
+// }
+console.log(process.argv);
+console.log(yargs(process.argv.slice(2)).argv);
 
