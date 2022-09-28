433cdf843d75beb6ddc5d61f099e49d98c4b0768
varnaraj <varnaraj28@gmail.com>
2022-09-28 10:36:55 +0530

    update

--- 1_notes_app/app.js
+++ 1_notes_app/app.js
@@ -1,10 +1,18 @@
-// const sum = require('./utils.js')
-// // const name ='varnaraj';
-// console.log(sum(3,5));
+// // const sum = require('./utils.js')
+// // // const name ='varnaraj';
+// // console.log(sum(3,5));
 
-const validator=require("validator");
-// const fs=require("fs");
-// const notes=require("./utils.js");
-// fs.writeFileSync("notes.js",notes());
-console.log(validator.isEmail('varnaa.gmail.com'));
-console.log(validator.isURL('https:/hii.io'));
+// const validator=require("validator");
+// // const fs=require("fs");
+// // const notes=require("./utils.js");
+// // fs.writeFileSync("notes.js",notes());
+// console.log(validator.isEmail('varnaa.gmail.com'));
+// console.log(validator.isURL('https:/hii.io'));
+
+// Importing module
+import { red } from 'chalk';
+
+// Printing the text
+console.log(red("aayush"))
+console.log(red.underline("aayush"))
+//console.log(red.underline.bold("GFG"))
