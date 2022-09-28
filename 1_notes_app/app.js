2830e94abe91969c962da8c52530de179e8e6d2d
varnaraj <varnaraj28@gmail.com>
2022-09-28 13:10:29 +0530

    update

--- 1_notes_app/app.js
+++ 1_notes_app/app.js
@@ -1,18 +1,21 @@
-// // const sum = require('./utils.js')
-// // // const name ='varnaraj';
-// // console.log(sum(3,5));
+// // // const sum = require('./utils.js')
+// // // // const name ='varnaraj';
+// // // console.log(sum(3,5));
 
-// const validator=require("validator");
-// // const fs=require("fs");
-// // const notes=require("./utils.js");
-// // fs.writeFileSync("notes.js",notes());
-// console.log(validator.isEmail('varnaa.gmail.com'));
-// console.log(validator.isURL('https:/hii.io'));
+// // const validator=require("validator");
+// // // const fs=require("fs");
+// // // const notes=require("./utils.js");
+// // // fs.writeFileSync("notes.js",notes());
+// // console.log(validator.isEmail('varnaa.gmail.com'));
+// // console.log(validator.isURL('https:/hii.io'));
 
-// Importing module
-import chalk from 'chalk';
+// // Importing module
+ import chalk from 'chalk';
 
-// Printing the text
-console.log(chalk.red("aayush"))
-console.log(chalk.green.bold.dim.inverse("aayush"))
-//console.log(red.underline.bold("GFG"))
+// // Printing the text
+ console.log(chalk.red("aayush"))
+ console.log(chalk.blue.bold.dim.inverse("aayssush"))
+// //console.log(red.underline.bold("GFG"))
+
+
+
