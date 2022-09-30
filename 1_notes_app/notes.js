6b501493ecff43bbfe1ecb36fb01a1807f5853ef
varnaraj <varnaraj28@gmail.com>
2022-09-30 09:37:36 +0530

    Update notes.js

--- 1_notes_app/notes.js
+++ 1_notes_app/notes.js
@@ -29,3 +29,22 @@
    
 // yargs.parse() // To set above changes
 
+const fs=require("fs");
+const getNotes=function(){
+    return "Your notes";
+}
+
+const addNotes=function(title,body){
+    const notes = loadNotes();
+}
+
+const loadNotes= function(){
+    const dataBuffer=fs.readFileSync("notes.json");
+    const dataJSON=dataBuffer.toString();
+    return JSON.parse(dataJSON);
+}
+
+module.exports={
+    getNotes:getNotes,
+    addNotes:addNotes
+}
