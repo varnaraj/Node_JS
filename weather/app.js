7061d6ea0f58f8aa3174f5cfd132035eb0b2ac65
varnaraj <varnaraj28@gmail.com>
2022-10-02 22:58:27 +0530

    Update app.js

--- weather/app.js
+++ weather/app.js
@@ -1,10 +1,1 @@
-console.log("Starting")
-setTimeout(()=>{
-    console.log("2 second timer")
-},2000);
-
-setTimeout(()=>{
-    console.log("0 second timer")
-},0);
-console.log("Stoping")
-
+require 
