9cc4e33e44878d781eb4e3b87b5ae6cb5de68d14
varnaraj <varnaraj28@gmail.com>
2022-10-02 23:22:02 +0530

    update

--- weather/app.js
+++ weather/app.js
@@ -1,1 +1,9 @@
-require 
+const request=require('request');
+
+const url="http://api.weatherstack.com/current?access_key=c1c76a4d1c54c17505a50cb32882694d&query=9.6615,80.0255";
+
+request({url:url},(error,response)=>{
+    data=JSON.parse(response.body);
+
+    console.log(data.location);
+})
